c6d97ac8373c59536f3f0127131c360cb6fea0dc
Nitish Kumar Jha <106099275+Nitish-hack@users.noreply.github.com>
2023-09-27 01:34:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -99,6 +99,7 @@ function App() {
         }
       </div>
       <ToastContainer position="bottom-center" />
+         <p style={{position:"fixed", bottom:"10px",left:"10px"}}>&copy; 2023 Nitish Kumar Jha. All rights reserved.</p>
     </>
   );
 }
